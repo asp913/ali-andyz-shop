42c2f5a21aed2688f569bbe0dcff082c7be7964b
asp913 <andrew@safechoicehvacnv.com>
2025-09-02 12:05:49 -0700

    Update and rename vercel.json to app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{maxWidth:960,margin:"64px auto",padding:"0 24px"}}>
+      <h1>Ali + Andy Z</h1>
+      <p>Now rendering via Next.js App Router ✅</p>
+    </main>
+  );
+}
