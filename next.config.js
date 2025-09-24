50cdf7a5450a8e5297858d1dfb77ab8511531fe6
asp913 <andrew@safechoicehvacnv.com>
2025-09-24 07:36:36 +0000

    Create clean next.config.js using echo commands

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
+            key: "Content-Security-Policy",
             value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.builder.io https://js.stripe.com; object-src 'none';"
           },
         ],
