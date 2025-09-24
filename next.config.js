897320d1fa74ed31f9581233199a1d9cefa1cf04
asp913 <andrew@safechoicehvacnv.com>
2025-09-24 08:00:05 +0000

    Remove all CSP restrictions to unblock Builder.io

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.builder.io https://js.stripe.com; object-src 'none';"
-          },
-        ],
-      },
-    ];
-  },
   experimental: {
     typedRoutes: true,
   },
